2fe5cfbf68ac0eb3d367d2fccd596fa43be052ce
Guhirwa <christianguhirwa74@gmail.com>
2025-06-09 16:27:28 +0200

    UPLOADING PROGRESS 	- Finishing styling my page 	- Implementing the logic to capture the actual date (Month-D-YYY) and display it

---
+++ app.js
@@ -0,0 +1,18 @@
+// 154ce9c4290971119d1e19bac1a4e4ad
+
+const date = document.getElementById('date');
+const city = document.getElementById('city');
+const temp = document.getElementById('temp');
+const tempImg = document.getElementById('tempImg');
+const description = document.getElementById('description');
+const tempMax = document.getElementById('tempMax');
+const tempMin = document.getElementById('tempMin');
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+let dateObj = new Date();
+let month = months[dateObj.getUTCMonth()];
+let day = dateObj.getUTCDate() - 1;
+let year = dateObj.getUTCFullYear();
+
+date.innerHTML = `${month} ${day} ${year}`
